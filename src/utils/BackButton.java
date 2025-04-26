1d5394a8d857a470754ac265c357198bedc2801d
xiaow <xiaow@10.101.147.114>
2025-04-26 23:05:31 +0800

    added back button

--- src/utils/BackButton.java
+++ src/utils/BackButton.java
@@ -7,12 +7,12 @@ import java.awt.*;
 public class BackButton extends JButton {
     
     public BackButton() {
-        this.setText("Back");
         this.initializeButton();
         this.addActionListener(e -> FrameManager.navigateBack());
     }
     
     private void initializeButton() {
+        this.setText("Back");
         this.setBounds(10, 10, 80, 30);
         this.setFont(new Font("Arial", Font.PLAIN, 14));
         this.setFocusable(false);
